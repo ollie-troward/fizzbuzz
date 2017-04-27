ed1ee92b599ff04bdb3bc8f6cf0d6b7a2c82388e
Ollie Troward <ollie@ollietroward.com>
2017-04-27 08:52:58 +0100

    Unhappy path test for reaching the maximum fizz buzz and nice message if the server is down

--- client/fizzbuzz.py
+++ client/fizzbuzz.py
@@ -7,11 +7,15 @@ import requests
 @click.option('--limit', help='Limit of numbers on a page.')
 def run(url, page, limit):
     parameters = {'page': page, 'limit': limit}
-    response = requests.get(url, params=parameters)
-    results = response.json()
-
-    for result in results:
-        click.echo("Number: %s, Favourite: %s" % (result['number'], result['favourite']))
+    try:
+        response = requests.get(url, params=parameters)
+        results = response.json()
+        for result in results:
+            click.echo("Number: %s, Favourite: %s" % (result['number'], result['favourite']))
+    except requests.ConnectionError as e:
+        click.echo(e)
+    except ValueError as e:
+        click.echo(response.content)
 
 if __name__ == '__main__':
     run()
--- client/fizzbuzz_test.py
+++ client/fizzbuzz_test.py
@@ -2,10 +2,16 @@ import click
 from click.testing import CliRunner
 from fizzbuzz import run
 
+def setup():
+    runner = CliRunner()
+    result = runner.invoke(run, ['http://localhost:3000'])
+    if "Connection refused" in result.output:
+        print("Client failed to connect, be sure the server is running.")
+
 def test_retrieve_default_fizzbuzz():
     runner = CliRunner()
     result = runner.invoke(run, ['http://localhost:3000'])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, "Unexpected exit code"
     assert "Number: 1, Favourite: False" in result.output
     assert "Number: Fizz, Favourite: False" in result.output
     assert "Number: Buzz, Favourite: False" in result.output
@@ -13,7 +19,7 @@ def test_retrieve_default_fizzbuzz():
 def test_retrieve_fizzbuzz_with_limit():
     runner = CliRunner()
     result = runner.invoke(run, ['http://localhost:3000', '--limit=1'])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, "Unexpected exit code"
     assert "Number: 1, Favourite: False" in result.output
     assert "Number: Fizz, Favourite: False" not in result.output
     assert "Number: Buzz, Favourite: False" not in result.output
@@ -21,14 +27,22 @@ def test_retrieve_fizzbuzz_with_limit():
 def test_retrieve_fizzbuzz_with_page_number():
     runner = CliRunner()
     result = runner.invoke(run, ['http://localhost:3000', '--limit=5', '--page=67'])
-    assert result.exit_code == 0
+    assert result.exit_code == 0, "Unexpected exit code"
     assert "Number: 331, Favourite: False" in result.output
     assert "Number: 332, Favourite: False" in result.output
     assert "Number: Fizz, Favourite: False" in result.output
     assert "Number: 334, Favourite: False" in result.output
     assert "Number: Buzz, Favourite: False" in result.output
 
+def test_reach_above_maximum():
+    runner = CliRunner()
+    result = runner.invoke(run, ['http://localhost:3000', '--limit=500', '--page=10000000000'])
+    assert result.exit_code == 0, "Unexpected exit code"
+    assert "FizzBuzz not found" in result.output
+
 if __name__ == '__main__':
+    setup()
     test_retrieve_default_fizzbuzz()
     test_retrieve_fizzbuzz_with_limit()
     test_retrieve_fizzbuzz_with_page_number()
+    test_reach_above_maximum()
